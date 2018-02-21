90e4bdebe032712eeef5b7e9e373866b01f3f5bd
tejismanp <tejismanp001@gmail.com>
2018-02-21 12:25:53 +0530

    mailcleaning with offer cleaning added

--- Mail_Cleaner.py
+++ Mail_Cleaner.py
@@ -6,7 +6,7 @@ def mail_cleaner_main():
 	print("\nMail Cleaning starting .. ")
 	df = pd.read_csv(config.raw_data_csv, encoding='ISO-8859-1')
 	df['Contents'] = df['Contents'].apply(functionalZone)
-		
+#	df['Offer'] = df['Offer'].apply()	
 	df.to_csv(config.raw_data_csv,index=False, encoding = "utf-8")	
 	print("Mail cleaning completed ...")
 
