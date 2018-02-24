a8ef1a7e3ae1a539b4513eff083e87acddeaeb81
dev <mailakshayb1@gmail.com>
2018-02-24 23:05:39 +0530

    DecodeCleaner as python.os script

--- DecodeCleaner.py
+++ DecodeCleaner.py
@@ -1,4 +1,4 @@
+# -*- coding: utf-8 -*-
+
 import os
-
-
-print("tr -cd  '\11\12\15\40-\176'  < Data/garbage.csv > Data/raw_data2.csv")
+os.system(f'''tr -cd "\11\12\15\40-\176" < Data/garbage.csv > Data/raw_data1.csv''')
